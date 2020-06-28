45aea284187dc2c0381075f98f8ecbcf24052eb2
Omar <oemyoem55@gmail.com>
2020-06-28 14:27:48 +0500

    Added Rectangle to the Content Area

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -4,11 +4,22 @@ import mummy from "../../Assets/mummy.png";
 import "./Content.css";
 
 function Content(porps) {
-  return <div className="content" onClick={porps.closemenu}>
-    <div className="mummy-wrapper">
-      <img src={mummy} className="mummy" alt="mummy"></img>
+  return (
+    <div className="content" onClick={porps.closemenu}>
+      <div className="mummy-wrapper">
+        <img src={mummy} className="mummy" alt="mummy"></img>
+        <div className="yellow-box-wrapper">
+          <svg width="150" height="500">
+            <rect
+              width="150"
+              height="500"
+              className="yellow-box"
+            />
+          </svg>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default Content;
