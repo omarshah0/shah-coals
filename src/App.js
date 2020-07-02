adf61b75b6457dea1ef51c6ce397ffe333aeab29
Omar <oemyoem55@gmail.com>
2020-07-02 19:46:29 +0500

    Added DropShadow Modal and Info Box for Mobile Device

--- src/App.js
+++ src/App.js
@@ -7,15 +7,21 @@ import Footer from "./Components/Footer/Footer";
 import SideDrawer from "./Components/Mobile Drawer/SideDrawer";
 import Coal from "./Components/Coal/Coal";
 
+import m416 from "./Assets/m416.png";
+
 function App() {
   const [menuToggle, setMenuToggle] = useState(false);
+  const [coalInfo, setCoalInfo] = useState(false);
 
   function toggleMenu() {
     setMenuToggle(!menuToggle);
   }
   function closeMenuOnly() {
     setMenuToggle(false);
-    console.log("CLICKEEEED");
+  }
+
+  function toggleCoalInfo() {
+    setCoalInfo(!coalInfo);
   }
 
   return (
@@ -30,9 +36,43 @@ function App() {
       </CSSTransition>
       <Header openDrawer={toggleMenu} />
       <Content closemenu={closeMenuOnly} />
-      <Coal coalName="BK-6" moisture="10" ash="3.14" sulfur="64" oxygen="30"/>
-      <Coal coalName="BK-2" moisture="30" ash="12.14" sulfur="34" oxygen="70"/>
-      <Coal coalName="BK-4" moisture="20" ash="34" sulfur="87" oxygen="40"/>
+      <Coal
+        coalInfoModal={toggleCoalInfo}
+        coalName="BK-6"
+        coalSize="6-Footy"
+        moisture="10"
+        ash="3.14"
+        sulfur="64"
+        oxygen="30"
+      />
+      <Coal
+        coalInfoModal={toggleCoalInfo}
+        coalName="BK-2"
+        coalSize="2-Footy"
+        moisture="30"
+        ash="12.14"
+        sulfur="34"
+        oxygen="70"
+      />
+      <Coal
+        coalInfoModal={toggleCoalInfo}
+        coalName="BK-4"
+        coalSize="4-Footy"
+        moisture="20"
+        ash="34"
+        sulfur="87"
+        oxygen="40"
+      />
+      {coalInfo ? (
+        <div className="backdrop">
+          <div className="coal-info" onClick={toggleCoalInfo}>
+            <img src={m416} style={ {width: "80%" , marginTop: "2rem"}} alt="m416"></img>
+            <h1 className="cola">PICTUER OF COAL</h1>
+            <h1 className="cola">NAME OF COAL</h1>
+            <p className="cola">INFO OF COAL</p>
+          </div>
+        </div>
+      ) : null}
       <Footer />
     </div>
   );
--- src/Components/Coal/Coal.js
+++ src/Components/Coal/Coal.js
@@ -6,30 +6,32 @@ import "./Coal.css";
 
 function Coal(props) {
   return (
-    <div className="coal-container">
-        <img src={m416} className="m416" alt="m416"></img>
+    <div className="coal-container" onClick={props.coalInfoModal}>
+      <img src={m416} className="m416" alt="m416"></img>
       <div className="chart">
-        <h2>{props.coalName}</h2>
+        <h2>
+          {props.coalName} <span className="coal-size">({props.coalSize})</span>
+        </h2>
         <li>
-          <h5>Moisture</h5>
+          <h5>Moisture - {props.moisture}%</h5>
           <span className="bar">
             <span style={{ width: props.moisture + "%" }}></span>
           </span>
         </li>
         <li>
-          <h5>Ash</h5>
+          <h5>Ash - {props.ash}%</h5>
           <span className="bar">
             <span style={{ width: props.ash + "%" }}></span>
           </span>
         </li>
         <li>
-          <h5>Sulfur</h5>
+          <h5>Sulfur - {props.sulfur}%</h5>
           <span className="bar">
             <span style={{ width: props.sulfur + "%" }}></span>
           </span>
         </li>
         <li>
-          <h5>Oxygen</h5>
+          <h5>Oxygen - {props.oxygen}%</h5>
           <span className="bar">
             <span style={{ width: props.oxygen + "%" }}></span>
           </span>
